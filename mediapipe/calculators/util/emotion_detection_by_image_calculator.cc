049b501be2f7852a6bcae6256948ddee2fbeccb4
tangzhiqiang <tangzhiqiang@51talk.com>
2024-07-31 11:00:15 +0800

    [自定义emotion-detection]完成各种功能封装:裁剪,计算rect,运行推理

--- mediapipe/calculators/util/emotion_detection_by_image_calculator.cc
+++ mediapipe/calculators/util/emotion_detection_by_image_calculator.cc
@@ -43,6 +43,9 @@
 #include <vector>
 #include <mutex>
 
+// #define OUTPUT_RESULT_VALUE
+// #define RENDER_CROP_FACE
+
 namespace mediapipe {
 
 namespace {
@@ -86,6 +89,10 @@ private:
   absl::Status CreateRenderTargetCpu(
       CalculatorContext* cc, std::unique_ptr<cv::Mat>& image_mat,
       ImageFormat::Format* target_format);
+
+  void dealWithDetection(const Detection& detection, cv::Mat& CVMat);
+  cv::Rect calculateBBoxRect(const LocationData& location_data, int32_t width, int32_t height);
+  void runTensorWithMat(const cv::Mat& face_mat);
 private:
 
   EmotionDetectionByImageCalculatorOptions m_options;
@@ -101,175 +108,99 @@ private:
   int32_t m_img_height=0;
   int32_t m_img_width=0;
 
-  float m_emotion_threshold=0.85;
+  float m_emotion_threshold=0.9;
   int32_t m_last_id = 4;
 
+#ifdef RENDER_CROP_FACE
   std::thread m_opencv_render_thread;
   std::mutex m_pMutex;
   cv::Mat m_will_render_mat;
-
+#endif
 };
 REGISTER_CALCULATOR(EmotionDetectionByImageCalculator);
 
-absl::Status EmotionDetectionByImageCalculator::GetContract( CalculatorContract* cc) 
-{
-  RET_CHECK(cc->Inputs().HasTag(kDetectionsTag))
-      << "None of the input streams are provided.";
-
-  if (cc->Inputs().HasTag(kImageFrameTag)) 
+  absl::Status EmotionDetectionByImageCalculator::GetContract( CalculatorContract* cc) 
   {
-    cc->Inputs().Tag(kImageFrameTag).Set<ImageFrame>();
-    printf("will process detections 0\n");
-  }
+    RET_CHECK(cc->Inputs().HasTag(kDetectionsTag))
+        << "None of the input streams are provided.";
 
-  if (cc->Inputs().HasTag(kDetectionsTag)) 
-  {
-    cc->Inputs().Tag(kDetectionsTag).Set<std::vector<Detection>>();
-    printf("will process detections 1\n");
-  }
-  return absl::OkStatus();
-}
-
-absl::Status EmotionDetectionByImageCalculator::Open(CalculatorContext* cc) {
-  cc->SetOffset(TimestampDiff(0));
-  m_options = cc->Options<EmotionDetectionByImageCalculatorOptions>();
-  ABSL_LOG(INFO) << "will load emotion model.";
-  loadEmotionModel();
-  m_opencv_render_thread=std::thread([this](){
-    cv::namedWindow("emotion detection", cv::WINDOW_AUTOSIZE);  
-    cv::Mat frameBuff;
-    while (true) {
-      if (m_will_render_mat.empty())
-      {
-        cv::waitKey(50);
-        continue;
-      }
-      
-      if (m_pMutex.try_lock()) {
-        frameBuff=m_will_render_mat;
-        m_pMutex.unlock();
-      }
-
-      cv::imshow("emotion detection", m_will_render_mat);
-      cv::waitKey(50);
-    }
-  });
-  
-  return absl::OkStatus();
-}
-
-absl::Status EmotionDetectionByImageCalculator::Process(CalculatorContext* cc) 
-{
-  if (cc->Inputs().Tag(kDetectionsTag).IsEmpty())
-  {
-    m_last_id = -1;//absent
-    CalculatorGraph::CreateAndGetGlobaData()->emotion_type=(EEmotionType)m_last_id;
-    return absl::OkStatus();
-  }
-  const bool has_detection_from_vector =
-      cc->Inputs().HasTag(kDetectionsTag) &&
-      !cc->Inputs().Tag(kDetectionsTag).Get<std::vector<Detection>>().empty();
-  
-  if (!has_detection_from_vector) {
-    return absl::OkStatus();
-  }
-  const auto& input_img = cc->Inputs().Tag(kImageFrameTag).Get<ImageFrame>();
-  if(m_img_width==0||m_img_height==0)
-  {
-    printf("input image width and height [%d %d] \n",
-            input_img.Width(),input_img.Height());
-    m_img_height = input_img.Height();
-    m_img_width = input_img.Width();
-  }
-  cv::Mat CVMat = formats::MatView(&input_img);
-  if (has_detection_from_vector) {
-    for (const auto& detection : cc->Inputs().Tag(kDetectionsTag)
-                                          .Get<std::vector<Detection>>()) {
-      auto &bboxC = detection.location_data().relative_bounding_box();
-
-      int32_t leftTopX = int(bboxC.xmin() * m_img_width);
-      if(leftTopX<0) leftTopX=0;
-      int32_t leftTopY = int(bboxC.ymin() * m_img_height);
-      if(leftTopY<0) leftTopY=0;
-      int32_t rightBottomX = int((bboxC.xmin() + bboxC.width()) * m_img_width);
-      if(rightBottomX>m_img_width) rightBottomX=m_img_width;
-      int32_t rightBottomY = int((bboxC.ymin() + bboxC.height()) * m_img_height);
-      if (rightBottomY>m_img_height) rightBottomY=m_img_height;
-      
-      
-      
-      // printf("leftTopX:%d leftTopY:%d width:%d height:%d\n",
-                // leftTopX,leftTopY,rightBottomX-leftTopX,rightBottomY-leftTopY);
-      //! 截取面部区域
-      cv::Mat faceMat = CVMat(cv::Rect(leftTopX, leftTopY, 
-                                rightBottomX-leftTopX, rightBottomY-leftTopY));
-      
-      // 将面部区域调整为固定大小
-      cv::Mat fixed_size_face;
-      resize(faceMat, fixed_size_face, cv::Size(64, 64));
-      // std::cout<<"mat channel 0: "<<faceMat.channels()<<std::endl;
-      cv::cvtColor(fixed_size_face, fixed_size_face, cv::COLOR_BGR2RGB);
-      // std::cout<<"mat channel 1: "<<fixed_size_face.channels()<<std::endl;
-
-      // 进行检测
-      fixed_size_face.convertTo(fixed_size_face, CV_32FC3, 1.0 / 255.0); // 归一化
-      if (m_pMutex.try_lock()) {
-        m_will_render_mat=fixed_size_face;
-        m_pMutex.unlock();
-      }
-      // std::vector<float> input_data(fixed_size_face.begin<float>(), fixed_size_face.end<float>());
-      // 扩展维度
-      std::vector<cv::Mat> input_data = { fixed_size_face };
-
-      int input_details_tensor_index = m_input_details[0];
-      int output_details_tensor_index = m_output_details[0];
-      // Set the input tensor
-          // 获取输入张量
-      TfLiteTensor* input_tensor = m_interpreter->tensor(input_details_tensor_index);
-      // float* input_tensor = m_interpreter->typed_tensor<float>(input_details_tensor_index);
-      // std::copy(input_data.begin(), input_data.end(), input_tensor); //! 一维数据
-      // 将输入数据赋值给模型输入//! 二维数据
-      std::memcpy(input_tensor->data.f, fixed_size_face.data, fixed_size_face.total() * fixed_size_face.elemSize());
-      // 运行推理
-      TfLiteStatus status_code = kTfLiteOk;
-      tflite::Subgraph* subgraph = m_interpreter->subgraph(0);
-      status_code = subgraph->Invoke();
-      // std::cout<<"status code: "<<status_code<<std::endl;
-      for (int tensor_index : subgraph->outputs()) {
-        subgraph->EnsureTensorDataIsReadable(tensor_index);
-      }
-      // Get the output tensor
-      auto tensor = m_interpreter->subgraph(0)->tensor(output_details_tensor_index);
-      float* result = (float*)tensor->data.raw;
-      // 处理输出数据
-      // 找到最大值及其索引
-      int num_output_elements = m_interpreter->tensor(output_details_tensor_index)->bytes / sizeof(float);
-      // std::cout<<"num_output_elements: "<<num_output_elements<<std::endl;
-      auto max_it = std::max_element(result, result + num_output_elements);
-      float max_value = *max_it;
-      int result_index = std::distance(result, max_it);
-      // std::cout << "Predicted class: " << result_index << ", Probability: " << max_value << std::endl;
-#if 1
-    for (size_t i = 0; i < num_output_elements; i++)
+    if (cc->Inputs().HasTag(kImageFrameTag)) 
     {
-      printf("result_index=%d value=%f \n",i,result[i]);
+      cc->Inputs().Tag(kImageFrameTag).Set<ImageFrame>();
+      printf("will process detections 0\n");
     }
-#endif
-      static int last_index=2;
-      if (max_value >= m_emotion_threshold) {
-          last_index = result_index;
-          m_last_id = result_index;
-      } else {
-          m_last_id = result_index;
-      }
-      CalculatorGraph::CreateAndGetGlobaData()->emotion_type=(EEmotionType)m_last_id;
-      // m_last_id = 4;
-      break;
+
+    if (cc->Inputs().HasTag(kDetectionsTag)) 
+    {
+      cc->Inputs().Tag(kDetectionsTag).Set<std::vector<Detection>>();
+      printf("will process detections 1\n");
     }
+    return absl::OkStatus();
   }
 
-  return absl::OkStatus();
-}
+  absl::Status EmotionDetectionByImageCalculator::Open(CalculatorContext* cc) {
+    cc->SetOffset(TimestampDiff(0));
+    m_options = cc->Options<EmotionDetectionByImageCalculatorOptions>();
+    ABSL_LOG(INFO) << "will load emotion model.";
+    loadEmotionModel();
+  #ifdef RENDER_CROP_FACE
+    m_opencv_render_thread=std::thread([this](){
+      cv::namedWindow("emotion detection", cv::WINDOW_AUTOSIZE);  
+      cv::Mat frameBuff;
+      while (true) {
+        if (m_will_render_mat.empty())
+        {
+          cv::waitKey(50);
+          continue;
+        }
+        if (m_pMutex.try_lock()) {
+          frameBuff=m_will_render_mat;
+          m_pMutex.unlock();
+        }
+        cv::imshow("emotion detection", m_will_render_mat);
+        cv::waitKey(50);
+      }
+    });
+  #endif
+
+    return absl::OkStatus();
+  }
+
+  absl::Status EmotionDetectionByImageCalculator::Process(CalculatorContext* cc) 
+  {
+    if (cc->Inputs().Tag(kDetectionsTag).IsEmpty())
+    {
+      m_last_id = -1;//absent
+      CalculatorGraph::CreateAndGetGlobaData()->emotion_type=(EEmotionType)m_last_id;
+      return absl::OkStatus();
+    }
+    const bool has_detection_from_vector =
+        cc->Inputs().HasTag(kDetectionsTag) &&
+        !cc->Inputs().Tag(kDetectionsTag).Get<std::vector<Detection>>().empty();
+    
+    if (!has_detection_from_vector) {
+      return absl::OkStatus();
+    }
+    const auto& input_img = cc->Inputs().Tag(kImageFrameTag).Get<ImageFrame>();
+    if(m_img_width==0||m_img_height==0)
+    {
+      printf("input image width and height [%d %d] \n",
+              input_img.Width(),input_img.Height());
+      m_img_height = input_img.Height();
+      m_img_width = input_img.Width();
+    }
+    cv::Mat CVMat = formats::MatView(&input_img);
+    if (has_detection_from_vector)
+    {
+      for (const auto& detection : cc->Inputs().Tag(kDetectionsTag)
+                                            .Get<std::vector<Detection>>()) 
+      {
+        dealWithDetection(detection, CVMat);
+        break;
+      }
+    }
+    return absl::OkStatus();
+  }
 
 
   absl::Status EmotionDetectionByImageCalculator::loadEmotionModel(){
@@ -408,5 +339,95 @@ absl::Status EmotionDetectionByImageCalculator::CreateRenderTargetCpu(
   return absl::OkStatus();
 }
 
+
+void EmotionDetectionByImageCalculator::dealWithDetection(const Detection& detection, 
+                                                            cv::Mat& CVMat)
+{
+    auto matRect = calculateBBoxRect(detection.location_data(), m_img_width, m_img_height);
+      
+    // printf("leftTopX:%d leftTopY:%d width:%d height:%d\n",
+              // leftTopX,leftTopY,rightBottomX-leftTopX,rightBottomY-leftTopY);
+    //! 截取面部区域
+    cv::Mat faceMat = CVMat(matRect);
+    // 将面部区域调整为固定大小
+    cv::Mat fixed_size_face;
+    resize(faceMat, fixed_size_face, cv::Size(64, 64));
+    // std::cout<<"mat channel 0: "<<faceMat.channels()<<std::endl;
+    cv::cvtColor(fixed_size_face, fixed_size_face, cv::COLOR_BGR2RGB);
+    // std::cout<<"mat channel 1: "<<fixed_size_face.channels()<<std::endl;
+    // 进行检测
+    fixed_size_face.convertTo(fixed_size_face, CV_32FC3, 1.0 / 255.0); // 归一化
+#ifdef RENDER_CROP_FACE
+    if (m_pMutex.try_lock()) {
+      m_will_render_mat=fixed_size_face;
+      m_pMutex.unlock();
+    }
+#endif
+    runTensorWithMat(fixed_size_face);
+}
+
+  cv::Rect EmotionDetectionByImageCalculator::calculateBBoxRect(const LocationData& location_data, 
+                                                            int32_t width, int32_t height)
+  {
+    auto &bboxC = location_data.relative_bounding_box();
+    int32_t leftTopX = int(bboxC.xmin() * width);
+    if(leftTopX<0) leftTopX=0;
+    int32_t leftTopY = int(bboxC.ymin() * height);
+    if(leftTopY<0) leftTopY=0;
+    int32_t rightBottomX = int((bboxC.xmin() + bboxC.width()) * width);
+    if(rightBottomX>width) rightBottomX=width;
+    int32_t rightBottomY = int((bboxC.ymin() + bboxC.height()) * height);
+    if (rightBottomY>height) rightBottomY=height;
+    return cv::Rect(leftTopX, leftTopY, rightBottomX-leftTopX, rightBottomY-leftTopY);
+  }
+
+  void EmotionDetectionByImageCalculator::runTensorWithMat(const cv::Mat& face_mat)
+  {
+    // std::vector<float> input_data(face_mat.begin<float>(), face_mat.end<float>());
+    // std::vector<cv::Mat> input_data = { face_mat };
+    int input_details_tensor_index = m_input_details[0];
+    int output_details_tensor_index = m_output_details[0];
+    // Set the input tensor
+        // 获取输入张量
+    TfLiteTensor* input_tensor = m_interpreter->tensor(input_details_tensor_index);
+    // float* input_tensor = m_interpreter->typed_tensor<float>(input_details_tensor_index);
+    // std::copy(input_data.begin(), input_data.end(), input_tensor); //! 一维数据
+    // 将输入数据赋值给模型输入//! 二维数据
+    std::memcpy(input_tensor->data.f, face_mat.data, face_mat.total() * face_mat.elemSize());
+    // 运行推理
+    TfLiteStatus status_code = kTfLiteOk;
+    tflite::Subgraph* subgraph = m_interpreter->subgraph(0);
+    status_code = subgraph->Invoke();
+    // std::cout<<"status code: "<<status_code<<std::endl;
+    for (int tensor_index : subgraph->outputs()) {
+      subgraph->EnsureTensorDataIsReadable(tensor_index);
+    }
+    // Get the output tensor
+    auto tensor = m_interpreter->subgraph(0)->tensor(output_details_tensor_index);
+    float* result = (float*)tensor->data.raw;
+    // 处理输出数据
+    // 找到最大值及其索引
+    int num_output_elements = m_interpreter->tensor(output_details_tensor_index)->bytes / sizeof(float);
+    // std::cout<<"num_output_elements: "<<num_output_elements<<std::endl;
+    auto max_it = std::max_element(result, result + num_output_elements);
+    float max_value = *max_it;
+    int result_index = std::distance(result, max_it);
+    // std::cout << "Predicted class: " << result_index << ", Probability: " << max_value << std::endl;
+#ifdef OUTPUT_RESULT_VALUE
+  for (size_t i = 0; i < num_output_elements; i++)
+  {
+    printf("result_index=%d value=%f \n",i,result[i]);
+  }
+#endif
+    static int last_index=2;
+    if (max_value >= m_emotion_threshold) {
+        last_index = result_index;
+        m_last_id = result_index;
+    } else {
+        m_last_id = result_index;
+    }
+    CalculatorGraph::CreateAndGetGlobaData()->emotion_type=(EEmotionType)m_last_id;
+
+  }
 
 }  // namespace mediapipe
